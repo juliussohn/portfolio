734836a01da011803e4e621d47b0aa9422b4cf25
Julius Sohn <juliussohn@icloud.com>
2017-10-10 00:35:27 +0900

    new portfolio

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname+"/build/",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }]
+    }
+};
